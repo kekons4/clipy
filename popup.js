9c6fc985bcbd490a637add4f44957c5bec6e41ab
Keon Pourboghrat <kekons4@gmail.com>
2024-01-23 11:00:11 -0500

    published clpy v1.5.0 merging with main

--- popup.js
+++ popup.js
@@ -52,10 +52,16 @@ function generateListItem(copyData) {
             }
         }
 
+        // create imagepath link to original image
         const i = document.createElement("i");
-        i.classList = "fas fa-copy copyicon";
+        i.classList = "fas fa-external-link-alt imagepath";
+        const imageLink = document.createElement("a");
+        imageLink.href = src;
+        imageLink.target = "_blank";
+        imageLink.append(i);
+
     
-        msg.append(i);
+        msg.append(imageLink);
     
         const trash = document.createElement("i");
         trash.setAttribute("data-index", copyData.index);
